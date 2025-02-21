fc808091a56d4fb391c9a3345811b68a4e25f7b7
srushti toli <srushtitoli05@gmail.com>
2025-02-21 15:40:22 +0530

    final done

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,5 +1,4 @@
-const API_URL = "http://localhost:8080";
-
+const API_URL = "https://ecom-backend-vuj6.onrender.com";
 export async function login(credentials) {
   const response = await fetch(`${API_URL}/login`, {
     method: "POST",
--- frontend/src/api/cart.js
+++ frontend/src/api/cart.js
@@ -1,5 +1,4 @@
-const API_URL = "http://localhost:8080";
-
+const API_URL = "https://ecom-backend-vuj6.onrender.com";
 export async function addToCart(product) {
   const response = await fetch(`${API_URL}/cart/add`, {
     method: "POST",
